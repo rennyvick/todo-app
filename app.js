8031b9986b9a417acf473abad899026edbe79dc0
Student <Student>
2018-03-28 16:41:00 +0300

    my todo-app

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-console.log('my app')
+const form= document.querySelector('form')
+const taskInput = document.querySelector('input[name=taskName]')
+const dateInput = document.querySelector('input[name=taskDate]')
+const ul = document.querySelector('ul')
 
-const div = document.querySelector('.container')
+form.onsubmit = function(e) {
+   e.preventDefault()
 
- console.log(div)
+	let taskTitle = taskInput.value
+	let taskDate =dateInput.value
+
+
+
+	const li = document.createElement('li')
+	li.textContent =taskTitle
+	li.classList.add('list-group-item')
+	li.classList.add('d-flex')
+	li.classList.add('justify-content-between')
+	li.classList.add('align-itms-center')
+
+
+	const span= document.createElement('span')
+	span.textContent = taskDate
+	span.classList.add('badge')
+	span.classList.add('badge-primary')
+	span.classList.add('badge-pill')
+
+
+
+	ul.appendChild(li)
+	li.appendChild(span)
+
+
+}
