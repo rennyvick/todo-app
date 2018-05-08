c3129daae6358e4ab81bcc11d978adf45ebf162d
Student <Student>
2018-05-08 12:22:42 +0300

    my todo list modified

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
-const form= document.querySelector('form')
-const taskInput = document.querySelector('input[name=taskName]')
-const dateInput = document.querySelector('input[name=taskDate]')
-const ul = document.querySelector('ul')
-
-form.onsubmit = function(e) {
-   e.preventDefault()
-
-	let taskTitle = taskInput.value
-	let taskDate =dateInput.value
-
-
-
-	const li = document.createElement('li')
-	li.textContent =taskTitle
-	li.classList.add('list-group-item')
-	li.classList.add('d-flex')
-	li.classList.add('justify-content-between')
-	li.classList.add('align-itms-center')
-
-
-	const span= document.createElement('span')
-	span.textContent = taskDate
-	span.classList.add('badge')
-	span.classList.add('badge-primary')
-	span.classList.add('badge-pill')
-
-
-
-	ul.appendChild(li)
-	li.appendChild(span)
-
-
-}
+var app = new Vue({
+  el: '#app',
+  data: {
+    todos: [
+ 'my first todo item',
+ 'my second todo item',
+ 'my third todo item'
+    ],
+    item: 'To do item'
+  },
+  methods: {
+  	addTodo(){
+  		this.todos.push(this.item)
+  		this.item='';
+  	}
+  }
+})
